2a7d0fcb38ec4d66b5f306242d9719be49426273
Madhuri Battula <137879232+BattulaMadhuri@users.noreply.github.com>
2023-07-19 20:17:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function bulboff(){
+    document.getElementById("bulb").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShKPchYCVW0Il3UaCus5Qd0l_fyVdRcNVI7g&usqp=CAU";
+}
+function bulbon(){
+    document.getElementById("bulb").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW6Qh4EgZ5CTgqa8ysS6veQp9ujjb4xZNhf-fnaRIq7Tb3yIMv-a6h8Ao3iqT8VVB8CAE&usqp=CAU";
+}  
